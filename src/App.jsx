360132b740f17ee0bf0e7d850ad60be1f4adb2fa
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2025-09-07 10:03:25 +0530

    refactor: remove routing and LandingPage component from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import LandingPage from './pages/LandingPage.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage/>} ></Route>
-      </Routes>
-    </BrowserRouter>
+    <h1>HI</h1>
   )
 }
 
