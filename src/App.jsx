c7bf22af38725713864d6096268922d97f42b155
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2025-09-07 09:53:40 +0530

    feat: add routing with react-router-dom and create LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>SIH PROJECT</h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage/>} ></Route>
+      </Routes>
+    </Router>
   )
 }
 
