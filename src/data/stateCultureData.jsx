685b6b65eee5f834f690711336c1e4666428e34a
pirates-of-mern <connectpro.inc@gmail.com>
2025-09-18 21:10:02 +0530

    Update stateCultureData.jsx

--- src/data/stateCultureData.jsx
+++ src/data/stateCultureData.jsx
@@ -3,38 +3,38 @@ import uttarPradesh from "./states/uttarPradesh";
 import madhyaPradesh from "./states/madhyaPradesh";
 import rajasthan from "./states/rajasthan";
 import punjab from "./states/punjab";
-import andamanNicobar from "./states/andamanNicobar.jsx";
-import andhraPradesh from "./states/AndhraPradesh.jsx";
-import arunachalPradesh from "./states/arunachalPradesh.jsx";
-import assam from "./states/assam.jsx";
-import bihar from "./states/bihar.jsx";
-import chhattisgarh from "./states/chhattisgarh.jsx";
-import goa from "./states/goa.jsx";
-import chandigarh from "./states/chandigarh.jsx";
-import delhi from "./states/delhi.jsx";
-import gujarat from "./states/gujarat.jsx";
-import dadraNagarHaveliDamanDiu from "./states/dadraNagarHaveli.jsx";
-import himachalPradesh from "./states/himachalPradesh.jsx";
-import jammuKashmir from "./states/jammuKashmir.jsx";
-import karnataka from "./states/karnataka.jsx";
-import jharkhand from "./states/jharkhand.jsx";
-import haryana from "./states/haryana.jsx";
-import kerala from "./states/kerala.jsx";
-import ladakh from "./states/ladakh.jsx";
-import lakshadweep from "./states/lakshadweep.jsx";
-import maharashtra from "./states/maharashtra.jsx";
-import manipur from "./states/manipur.jsx";
-import meghalaya from "./states/meghalaya.jsx";
-import mizoram from "./states/mizoram.jsx";
-import nagaland from "./states/nagaland.jsx";
-import odisha from "./states/odisha.jsx";
-import sikkim from "./states/sikkim.jsx";
-import tamilNadu from "./states/tamilNadu.jsx";
-import telangana from "./states/telangana.jsx";
-import tripura from "./states/tripura.jsx";
-import uttarakhand from "./states/uttarakhand.jsx";
-import westBengal from "./states/westBengal.jsx";
-import puducherry from "./states/puducherry.jsx";
+import andamanAndNicobar from "./states/AndamanAndNicobarIslands";
+import andhraPradesh from "./states/AndhraPradesh";
+import arunachalPradesh from "./states/arunachalPradesh";
+import assam from "./states/Assam";
+import bihar from "./states/Bihar";
+import chhattisgarh from "./states/Chhattisgarh";
+import goa from "./states/Goa";
+import chandigarh from "./states/Chandigarh";
+import delhi from "./states/Delhi";
+import gujarat from "./states/Gujarat";
+import dadraNagarHaveliDamanDiu from "./states/DadraAndNagarHaveliAndDamanAndDiu";
+import himachalPradesh from "./states/HimachalPradesh.jsx";
+import jammuKashmir from "./states/JammuAndKashmir.jsx";
+import karnataka from "./states/Karnataka.jsx";
+import jharkhand from "./states/Jharkhand.jsx";
+import haryana from "./states/Haryana.jsx";
+import kerala from "./states/Kerala.jsx";
+import ladakh from "./states/Ladakh.jsx";
+import lakshadweep from "./states/Lakshadweep.jsx";
+import maharashtra from "./states/Maharashtra.jsx";
+import manipur from "./states/Manipur.jsx";
+import meghalaya from "./states/Meghalaya.jsx";
+import mizoram from "./states/Mizoram.jsx";
+import nagaland from "./states/Nagaland.jsx";
+import odisha from "./states/Odisha.jsx";
+import sikkim from "./states/Sikkim.jsx";
+import tamilNadu from "./states/TamilNadu.jsx";
+import telangana from "./states/Telangana.jsx";
+import tripura from "./states/Tripura.jsx";
+import uttarakhand from "./states/Uttarakhand.jsx";
+import westBengal from "./states/WestBengal.jsx";
+import puducherry from "./states/Puducherry.jsx";
 
 
 const stateCultureData = {
@@ -42,7 +42,7 @@ const stateCultureData = {
   "rajasthan": rajasthan,
     "madhya-pradesh": madhyaPradesh,
   "punjab": punjab,
-  "andaman-nicobar": andamanNicobar,
+  "andaman-and-nicobar-islands": andamanAndNicobar,
   "andhra-pradesh": andhraPradesh,
   "arunachal-pradesh": arunachalPradesh,
   "assam": assam,
@@ -52,9 +52,9 @@ const stateCultureData = {
   "chandigarh": chandigarh,
   "delhi": delhi,
   "gujarat": gujarat,
-  "dadra-nagar-haveli-daman-diu": dadraNagarHaveliDamanDiu,
+  "dadra-and-nagar-haveli": dadraNagarHaveliDamanDiu,
   "himachal-pradesh": himachalPradesh,
-  "jammu-kashmir": jammuKashmir,
+  "jammu-and-kashmir": jammuKashmir,
   "karnataka": karnataka,
   "jharkhand": jharkhand,
   "haryana": haryana,
@@ -77,3 +77,4 @@ const stateCultureData = {
 };
 
 export default stateCultureData;
+
