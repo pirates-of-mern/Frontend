3b8d99c9bd28851966cf25761aaee31c50a65a73
pirates-of-mern <connectpro.inc@gmail.com>
2025-09-18 20:22:49 +0530

    Update madhyaPradesh.jsx

--- src/data/states/madhyaPradesh.jsx
+++ src/data/states/madhyaPradesh.jsx
@@ -2,11 +2,26 @@
 const madhyaPradesh = {
   sites: [
     {
+      name: "Jahaz Mahal",
+      city: "Mandav",
+      description: "A historic palace shaped like a ship.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Jahaz_Mahal_02.jpg/640px-Jahaz_Mahal_02.jpg",
+    },
+    {
+      name: "The Great Stupa at Sanchi",
+      city: "Sanchi",
+      description:
+        "An ancient Buddhist complex and UNESCO World Heritage Site.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Stupa_3%2C_Sanchi_01.jpg/640px-Stupa_3%2C_Sanchi_01.jpg",
+    },
+    {
       name: "Kanha National Park",
       city: "Mandla",
       description: "A famous wildlife sanctuary and tiger reserve.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/6/6b/Kanha_National_Park.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Kanha_National_Park_-_panoramio.jpg/640px-Kanha_National_Park_-_panoramio.jpg",
     },
     {
       name: "Khajuraho Temples",
@@ -14,28 +29,23 @@ const madhyaPradesh = {
       description:
         "A UNESCO World Heritage Site known for its temples with intricate carvings.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/3/3e/Khajuraho_Temples.jpg",
-    },
-    {
-      name: "Sanchi Stupa",
-      city: "Sanchi",
-      description: "A Buddhist complex with ancient stupas and monuments.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/4/43/Sanchi_Stupa.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Homosexuality_in_Khajuraho_sculpture.jpg/640px-Homosexuality_in_Khajuraho_sculpture.jpg",
     },
   ],
   dance: [
     {
-      name: "Lavani",
-      description: "A folk dance form with energetic performances.",
-      image: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Lavani.jpg",
+      name: "Matki Dance",
+      description:
+        "A traditional dance of Malwa, performed by women balancing earthen pots.",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Jal_Mahotsav_Dance.jpg/640px-Jal_Mahotsav_Dance.jpg",
     },
     {
-      name: "Kalbelia",
+      name: "Rai Dance",
       description:
-        "A dance form of the Kalbelia community, known for its serpentine movements.",
+        "A tribal folk dance of Bundelkhand region performed during festivals.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d0/Kalbelia_Dance.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Kathak_dancer_Namrata_Rai_with_her_group.jpg/640px-Kathak_dancer_Namrata_Rai_with_her_group.jpg",
     },
   ],
   music: [
@@ -44,12 +54,13 @@ const madhyaPradesh = {
       description:
         "Traditional music that includes songs related to various customs and festivals.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/4/46/Folk_music.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/6_ancient_drum_types_of_Madhya_Pradesh%2C_Indian_subcontinent.jpg/640px-6_ancient_drum_types_of_Madhya_Pradesh%2C_Indian_subcontinent.jpg",
     },
     {
       name: "Dhol",
       description: "A traditional Indian drum used in folk music performances.",
-      image: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Dhol.jpg",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Dohol_%D8%AF%D9%87%D9%84_from_Khorasan.jpg/640px-Dohol_%D8%AF%D9%87%D9%84_from_Khorasan.jpg",
     },
   ],
   cuisine: [
@@ -57,13 +68,13 @@ const madhyaPradesh = {
       name: "Poha",
       description: "A popular breakfast dish made of flattened rice.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/4/43/Poha_with_tamarind.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Poha_dish.JPG/640px-Poha_dish.JPG",
     },
     {
       name: "Bhopali Gosht Korma",
       description: "A rich mutton curry, famous in the city of Bhopal.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/7/70/Gosht_Korma.jpg",
+        "https://omgies.com/wp-content/uploads/2016/02/20160213_132644-638x479.jpg",
     },
   ],
   attire: [
@@ -72,14 +83,14 @@ const madhyaPradesh = {
       description:
         "Traditional handwoven saree from Chanderi, known for its fine texture.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/6/63/Chanderi_Saree.jpg",
+        "https://www.chanderisaree.in/product_images/thumb_435/Stormcloud-Color-Organza-Chanderi-Katan-Silk-Saree-RA-797-A.jpeg",
     },
     {
       name: "Maheshwari Saree",
       description:
         "Famous sarees from the Maheshwar region, known for their unique weave and design.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/1/1e/Maheshwari_Saree.jpg",
+        "https://shop.gaatha.com/image/catalog/data/ravi%20chouhan/29-7-17/krishnakali--saree.jpg",
     },
   ],
   festival: [
@@ -88,13 +99,13 @@ const madhyaPradesh = {
       description:
         "A festival celebrated by women with rituals for the well-being of their husbands.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9c/Teej_Festival.jpg",
+        "https://static.toiimg.com/thumb/msid-102768209,width-400,resizemode-4/102768209.jpg",
     },
     {
       name: "Khajuraho Dance Festival",
       description: "An annual dance festival showcasing classical dance forms.",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/d/d4/Khajuraho_Dance_Festival.jpg",
+        "https://www.sahyogmantratours.com/images/blogs/khajuraho-dance-festival-blog-2-20241003142644-1_crop.jpg",
     },
   ],
 };
