a4c3dc693bb84980234d393286034feb2878edd7
pirates-of-mern <connectpro.inc@gmail.com>
2025-09-18 20:35:05 +0530

    Update stateCultureData.jsx

--- src/data/stateCultureData.jsx
+++ src/data/stateCultureData.jsx
@@ -3,12 +3,77 @@ import uttarPradesh from "./states/uttarPradesh";
 import madhyaPradesh from "./states/madhyaPradesh";
 import rajasthan from "./states/rajasthan";
 import punjab from "./states/punjab";
+import andamanNicobar from "./states/andamanNicobar.jsx";
+import andhraPradesh from "./states/AndhraPradesh.jsx";
+import arunachalPradesh from "./states/arunachalPradesh.jsx";
+import assam from "./states/assam.jsx";
+import bihar from "./states/bihar.jsx";
+import chhattisgarh from "./states/chhattisgarh.jsx";
+import goa from "./states/goa.jsx";
+import chandigarh from "./states/chandigarh.jsx";
+import delhi from "./states/delhi.jsx";
+import gujarat from "./states/gujarat.jsx";
+import dadraNagarHaveliDamanDiu from "./states/dadraNagarHaveli.jsx";
+import himachalPradesh from "./states/himachalPradesh.jsx";
+import jammuKashmir from "./states/jammuKashmir.jsx";
+import karnataka from "./states/karnataka.jsx";
+import jharkhand from "./states/jharkhand.jsx";
+import haryana from "./states/haryana.jsx";
+import kerala from "./states/kerala.jsx";
+import ladakh from "./states/ladakh.jsx";
+import lakshadweep from "./states/lakshadweep.jsx";
+import maharashtra from "./states/maharashtra.jsx";
+import manipur from "./states/manipur.jsx";
+import meghalaya from "./states/meghalaya.jsx";
+import mizoram from "./states/mizoram.jsx";
+import nagaland from "./states/nagaland.jsx";
+import odisha from "./states/odisha.jsx";
+import sikkim from "./states/sikkim.jsx";
+import tamilNadu from "./states/tamilNadu.jsx";
+import telangana from "./states/telangana.jsx";
+import tripura from "./states/tripura.jsx";
+import uttarakhand from "./states/uttarakhand.jsx";
+import westBengal from "./states/westBengal.jsx";
+import puducherry from "./states/puducherry.jsx";
+
 
 const stateCultureData = {
   "uttar-pradesh": uttarPradesh,
   "rajasthan": rajasthan,
     "madhya-pradesh": madhyaPradesh,
   "punjab": punjab,
+  "andaman-nicobar": andamanNicobar,
+  "andhra-pradesh": andhraPradesh,
+  "arunachal-pradesh": arunachalPradesh,
+  "assam": assam,
+  "bihar": bihar,
+  "chhattisgarh": chhattisgarh,
+  "goa": goa,
+  "chandigarh": chandigarh,
+  "delhi": delhi,
+  "gujarat": gujarat,
+  "dadra-nagar-haveli-daman-diu": dadraNagarHaveliDamanDiu,
+  "himachal-pradesh": himachalPradesh,
+  "jammu-kashmir": jammuKashmir,
+  "karnataka": karnataka,
+  "jharkhand": jharkhand,
+  "haryana": haryana,
+  "kerala": kerala,
+  "ladakh": ladakh,
+  "lakshadweep": lakshadweep,
+  "maharashtra": maharashtra,
+  "manipur": manipur,
+  "meghalaya": meghalaya,
+  "mizoram": mizoram,
+  "nagaland": nagaland,
+  "odisha": odisha,
+  "sikkim": sikkim,
+  "tamil-nadu": tamilNadu,
+  "telangana": telangana,
+  "tripura": tripura,
+  "uttarakhand": uttarakhand,
+  "west-bengal": westBengal,
+  "puducherry": puducherry,
 };
 
 export default stateCultureData;
