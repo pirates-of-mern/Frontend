0d3be9cf8ce579a40dc3c9ba38ae72de81fa1624
pirates-of-mern <connectpro.inc@gmail.com>
2025-09-18 20:16:49 +0530

    Update Chhattisgarh.jsx

---
+++ src/data/states/Chhattisgarh.jsx
@@ -0,0 +1,54 @@
+// chhattisgarh.js
+const chhattisgarh = {
+  sites: [
+    {
+      name: "Chitrakote Falls",
+      city: "Bastar",
+      description: "Known as the 'Niagara of India', a stunning waterfall on the Indravati River.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/15/Chitrakote_Falls.jpg",
+    },
+    {
+      name: "Bhoramdeo Temple",
+      city: "Kawardha",
+      description: "Ancient temple known for its exquisite sculptures and architecture.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/5/53/Bhoramdeo_Temple.jpg",
+    },
+  ],
+  dance: [
+    {
+      name: "Panthi",
+      description: "Folk dance performed by the Satnami community, showcasing devotion and valor.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Panthi_Dance.jpg",
+    },
+  ],
+  music: [
+    {
+      name: "Gendi",
+      description: "Traditional folk music performed during festivals and celebrations.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/61/Gendi_Music.jpg",
+    },
+  ],
+  cuisine: [
+    {
+      name: "Chana Samosa",
+      description: "Popular Chhattisgarhi snack made with spiced chickpeas.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Chana_Samosa.jpg",
+    },
+  ],
+  attire: [
+    {
+      name: "Kosa Silk Saree",
+      description: "Famous handwoven silk sarees with traditional patterns.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Kosa_Silk_Saree.jpg",
+    },
+  ],
+  festival: [
+    {
+      name: "Bastar Dussehra",
+      description: "Unique 75-day long festival celebrating local deities and tribal culture.",
+      image: "https://upload.wikimedia.org/wikipedia/commons/2/2b/Bastar_Dussehra.jpg",
+    },
+  ],
+};
+
+export default chhattisgarh;
