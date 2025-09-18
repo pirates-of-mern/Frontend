6dec19975054818d968ea0c7a8b402ab2953b6f8
pirates-of-mern <connectpro.inc@gmail.com>
2025-09-18 16:05:49 +0530

    Add files via upload

---
+++ src/data/stateCultureData.jsx
@@ -0,0 +1,14 @@
+// stateCultureData.js
+import uttarPradesh from "./states/uttarPradesh";
+import madhyaPradesh from "./states/madhyaPradesh";
+import rajasthan from "./states/rajasthan";
+import punjab from "./states/punjab";
+
+const stateCultureData = {
+  "uttar-pradesh": uttarPradesh,
+  "rajasthan": rajasthan,
+    "madhya-pradesh": madhyaPradesh,
+  "punjab": punjab,
+};
+
+export default stateCultureData;
