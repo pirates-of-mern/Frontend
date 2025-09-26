a1c750fcb14aba372b8cb595202ab7674cf3d401
pirates-of-mern <connectpro.inc@gmail.com>
2025-09-26 15:35:59 +0530

    Create modelsData.jsx

---
+++ src/models/modelsData.jsx
@@ -0,0 +1,23 @@
+export const models = [
+  {
+    id: 1,
+    name: "Taj Mahal",
+    slug: "tajmahal",
+    src: "/models/cvjxb8n6buo0-tajmahal.glb",
+    thumbnail: "/thumbnails/tajmahal.jpg",
+  },
+  {
+    id: 2,
+    name: "Qutub Minar",
+    slug: "qutubminar",
+    src: "/models/qutub_minar.glb",
+    thumbnail: "/thumbnails/qutubminar.jpg",
+  },
+  {
+    id: 3,
+    name: "Red Fort",
+    slug: "laalqila",
+    src: "/models/red_fort_model.glb",
+    thumbnail: "/thumbnails/red_fort.jpg",
+  },
+];
